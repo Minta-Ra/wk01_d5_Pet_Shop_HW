a6fd160a32170ec2d747b053976563fb1e42ba67
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-12 18:17:20 +0000

    Pet stock, get by breed and name, remove

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,6 @@
+# Import Python debugger
+import pdb # pdb.set_trace()
+
 # WRITE YOUR FUNCTIONS HERE
 
 
@@ -22,3 +25,42 @@ def get_pets_sold(pet_shop):
 
 def increase_pets_sold(pet_shop, sold):
     pet_shop["admin"]["pets_sold"] += sold
+
+
+# Both functions return number of pets in the shop
+def get_stock_count(pet_shop):
+    num_of_pets = 0
+    for pet in pet_shop["pets"]:
+        num_of_pets += 1
+    return num_of_pets
+# OR:
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+
+##########################################################################
+# This function covers both, test_all_pets_by_breed__found and not_found from pet_shop.py
+def get_pets_by_breed(pet_shop, breed):
+    # Empty list to store breeds
+    pets_by_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_by_breed.append(pet)  
+    return pets_by_breed
+
+
+# This function covers both, test_find_pet_by_name__returns_pet and returns_None from pet_shop.py
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+    # If pet name is not found
+    return None
+
+
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            pet_shop["pets"].remove(pet)
+
+
